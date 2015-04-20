229ed1d9ac0a9f7387e905d638d402ca581daea3
francozappa <antonioli.daniele@gmail.com>
2015-04-20 18:47:51 +0800

    Complete IP mapping

--- minicps/constants.py
+++ minicps/constants.py
@@ -4,44 +4,68 @@ minicps constants.
 L0 rings are isolated dicts.
 
 L1 network devices are divided into dicts according to the device type.
+L1 wireless clients connect to CONDUITS[ap_pcm]
 """
 
 
 L0_RING1 = {
     'plc': '192.168.0.10',
     'plcr': '192.168.0.11',
-    'rio': 'TODO',
-    'act1': 'TODO',
-    'sen1': 'TODO',
-    'ap': 'TODO',
-    'wireless_rio': 'TODO',
+    'rio': '192.168.0.12',
+    'rio_ap': '192.168.0.14',
+    'sens_wifi_client': '192.168.0.15',
 }
 
 L0_RING2 = {
     'plc': '192.168.0.20',
     'plcr': '192.168.0.21',
+    'rio': '192.168.0.22',
+    'rio_ap': '192.168.0.24',
+    'sens_wifi_client': '192.168.0.25',
 }
 
 L0_RING3 = {
     'plc': '192.168.0.30',
     'plcr': '192.168.0.31',
+    'rio': '192.168.0.32',
+    'rio_ap': '192.168.0.34',
+    'sens_wifi_client': '192.168.0.35',
 }
 
 L0_RING4 = {
     'plc': '192.168.0.40',
     'plcr': '192.168.0.41',
+    'rio': '192.168.0.42',
+    'rio_ap': '192.168.0.44',
+    'sens_wifi_client': '192.168.0.45',
 }
 
 L0_RING5 = {
     'plc': '192.168.0.50',
     'plcr': '192.168.0.51',
+    'rio': '192.168.0.52',
+    'rio_ap': '192.168.0.54',
+    'sens_wifi_client': '192.168.0.55',
+    'etap_vsd1': '192.168.0.56',
+    'vsd1': '192.168.0.57',
+    'etap_vsd2': '192.168.0.58',
+    'vsd2': '192.168.0.59',
 }
 
 L0_RING6 = {
     'plc': '192.168.0.60',
     'plcr': '192.168.0.61',
+    'rio': '192.168.0.62',
+    'rio_ap': '192.168.0.64',
+    'sens_wifi_client': '192.168.0.65',
 }
 
+L0_RING7 = {
+    'plc': '192.168.0.70',
+    'rio': '192.168.0.72',
+    'rio_ap': '192.168.0.74',
+    'sens_wifi_client': '192.168.0.75',
+}
 
 L1_PLCS_IP = {
     'plc1': '192.168.1.10',
@@ -56,26 +80,35 @@ L1_PLCS_IP = {
     'plc4r': '192.168.1.41',
     'plc5r': '192.168.1.51',
     'plc6r': '192.168.1.61',
+    # used as central hub
+    'plc7': '192.168.1.70',
 }
 
-L1_APS_IP = {
-    'ap1': 'TODO',  # MOXA
-    'ap2': 'TODO',
-    'ap3': 'TODO',
-    'ap4': 'TODO',
-    'ap5': 'TODO',
-    'ap6': 'TODO',
-    'ap_control_network': 'TODO',  # SCADA and Historian
-    'ap_hmi': 'TODO',
-}
 
-L3_SCADA = {
-}
-
-L3_Historian = {
+L1_WIFI_CLIENTS_IP = {
+    'c1': '192.168.1.12',
+    'c2': '192.168.1.22',
+    'c3': '192.168.1.32',
+    'c4': '192.168.1.42',
+    'c5': '192.168.1.52',
+    'c6': '192.168.1.62',
+    'c7': '192.168.1.72',
 }
 
 L2_HMI = {
+    'hmi': '192.168.1.100',
+    'wifi_client': '192.168.1.101',
+}
+
+CONDUITS = {
+    'firewall': '192.168.1.102',
+    'ap_pcm': '192.168.1.103',  # plant control network
+    'ap_dmz': '192.168.1.104',
+}
+
+L3_PLANT_NETWORK = {
+    'historian': '192.168.1.200',
+    'workstation': '192.168.1.201',
 }
 
 
@@ -92,18 +125,6 @@ PLCS_MAC = {
     'plc4r': '00:1D:9C:C8:BC:31',
     'plc5r': '00:1D:9C:C8:F4:B9',
     'plc6r': '00:1D:9C:C8:F5:DB',
+    'plc7': 'TODO',
 }
 PLCS = len(PLCS_MAC)
-
-APS_MAC = {
-    'ap1': 'TODO',  # MOXA
-    'ap2': 'TODO',
-    'ap3': 'TODO',
-    'ap4': 'TODO',
-    'ap5': 'TODO',
-    'ap6': 'TODO',
-    'ap_control_network': 'TODO',  # SCADA and Historian
-    'ap_hmi': 'TODO',
-}
-APS = len(APS_MAC)
-
