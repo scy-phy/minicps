d1ec208b6140094e4a578f0f55071bc49ebbe3cb
pgaulon <pierre.gaulon1@orange.fr>
2015-06-11 16:37:58 +0800

    hmi plc1 script files

---
+++ tests/generate_sensor_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import random
+
+nb_values = 100000
+sensor_file = open("../temp/monitoring_test/sensor.txt", 'w')
+maxi = 1600.0
+mini = 0.0
+for i in range(nb_values):
+    nb = random.uniform(mini, maxi)
+    sensor_file.write("{:3.2f}\n".format(nb))
+
+sensor_file.close()
