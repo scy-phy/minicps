a065c113b91842d987db580c7d24683a1fa21b38
pgaulon <pierre.gaulon1@orange.fr>
2015-07-22 16:38:09 +0800

    add logs

--- examples/swat/physical_process.py
+++ examples/swat/physical_process.py
@@ -58,11 +58,12 @@ class Tank:
     -P: list of output pumps tags
     -FIT_out: list of output flows tags
     -subprocess: number of subprocess in which the tank belongs
+    -tank_number: id of the tank in the subprocess
     -diameter: tank diameter (m)
     -timer: period in which the tank has to actualize its values (s)
     -timeout: period of activity (s)
     """
-    def __init__(self, FIT_in, MV, LIT, P, FIT_out, subprocess, diameter, timer, timeout):
+    def __init__(self, FIT_in, MV, LIT, P, FIT_out, subprocess, tank_number, diameter, timer, timeout):
         """
         constructor
         """
@@ -72,10 +73,12 @@ class Tank:
         self.__P = P
         self.__FIT_out = FIT_out
         self.__subprocess = subprocess
+        self.__id = tank_number
         self.__diameter = diameter
         self.__timer = timer
         self.__timeout = timeout
         self.__process = None
+        logger.info('PP - tank number %d in subprocess %d created' % (self.__id, self.__subprocess))
 
     def __del__(self):
         """
@@ -83,6 +86,7 @@ class Tank:
         """
         if(self.__process != None):
             self.__process.join()
+        logger.info('PP - tank number %d in subprocess %d removed' % (self.__id, self.__subprocess))
 
     def compute_new_flow_level(self, FIT_list, MV_list, current_flow, P_list, valve_diameter):
         """
@@ -121,38 +125,46 @@ class Tank:
             output_valves = []
         else:
             output_valves = None
+            logger.warn('PP - tank number %d in subprocess %d has no output valves' % (self.__id, self.__subprocess))
 
         for index in self.__FIT_in:
             value = read_single_statedb(self.__subprocess, index)
             if value is not None:
                 input_flows.append(select_value(value))
+            else:
+                logger.warn('PP - tank number %d in subprocess %d can\'t read %s' % (self.__id, self.__subprocess, index))
 
         for index in self.__MV:
             value = read_single_statedb(self.__subprocess, index)
             if value is not None:
                 input_valves.append(select_value(value))
+            else:
+                logger.warn('PP - tank number %d in subprocess %d can\'t read %s' % (self.__id, self.__subprocess, index))
 
         if self.__P is not None:
             for index in self.__P:
                 value = read_single_statedb(self.__subprocess, index)
                 if value is not None:
                     output_valves.append(select_value(value))
+                else:
+                    logger.warn('PP - tank number %d in subprocess %d can\'t read %s' % (self.__id, self.__subprocess, index))
 
         current_flow = read_statedb(NAME=self.__LIT)
-        print self.__subprocess
-        print current_flow[0][3]
+        logger.debug('PP - tank number %d in subprocess %d current flow: %f' % (self.__id, self.__subprocess, float(current_flow[0][3])))
         if current_flow is not None:
-            logger.debug('PP - read from state db %s' % current_flow[0][3])
-
             if self.__FIT_out is not None:
                 v = Toricelli(float(current_flow[0][3]))  # m/s
                 flow = v * power(valve_diameter/2.0, 2) * pi * 3600.0  # m^3/h
                 for index in self.__FIT_out:
                     update_statedb(flow, index)
-
+                    logger.debug('PP - tank number %d in subprocess %d output flow: %f written into DB' % (self.__id, self.__subprocess, flow))
+            else:
+                logger.warn('PP - tank number %d in subprocess %d has no output flows' % (self.__id, self.__subprocess))
             new_flow = self.compute_new_flow_level(input_flows, input_valves, float(current_flow[0][3]), output_valves, valve_diameter)
-            logger.debug('PP - write to state db %.4f' % new_flow)
             update_statedb(new_flow, self.__LIT)
+            logger.debug('PP - tank number %d in subprocess %d new flow: %f written into DB' % (self.__id, self.__subprocess, new_flow))
+        else:
+            logger.warn('PP - tank number %d in subprocess %d can\'t read %s' % (self.__id, self.__subprocess, self.__LIT))
 
     def action_wrapper(self, valve_diameter):
         """
@@ -169,6 +181,7 @@ class Tank:
         """
         self.__process = Process(target = self.action_wrapper, args = (valve_diameter,))
         self.__process.start()
+        logger.info('PP - tank number %d in subprocess %d started' % (self.__id, self.__subprocess))
 
 if __name__ == '__main__':
     """
@@ -176,7 +189,7 @@ if __name__ == '__main__':
     -constructs all the subprocess tanks
     -runs them in parallel
     """
-    tank1 = Tank(['AI_FIT_101_FLOW'], ['DO_MV_101_OPEN'], 'AI_LIT_101_LEVEL', ['DO_P_101_START'], ['AI_FIT_201_FLOW'], 1, TANK_DIAMETER, TIMER, TIMEOUT)
-    tank2 = Tank(['AI_FIT_201_FLOW'], ['DO_MV_201_OPEN'], 'AI_LIT_301_LEVEL', None, None, 2, TANK_DIAMETER, TIMER, TIMEOUT)
+    tank1 = Tank(['AI_FIT_101_FLOW'], ['DO_MV_101_OPEN'], 'AI_LIT_101_LEVEL', ['DO_P_101_START'], ['AI_FIT_201_FLOW'], 1, 1, TANK_DIAMETER, TIMER, TIMEOUT)
+    tank2 = Tank(['AI_FIT_201_FLOW'], ['DO_MV_201_OPEN'], 'AI_LIT_301_LEVEL', None, None, 2, 1, TANK_DIAMETER, TIMER, TIMEOUT)
     tank1.start(VALVE_DIAMETER)
     tank2.start(VALVE_DIAMETER)
