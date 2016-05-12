0f34bd8bd3a73d7311cecc20f6f6405665a1ebb8
francozappa <antonioli.daniele@gmail.com>
2016-05-12 10:34:02 +0800

    Create state and testing suite

---
+++ minicps/state.py
@@ -0,0 +1,44 @@
+"""
+state.py
+"""
+
+import sqlite3
+# import redis
+# import pymongo
+
+
+# public {{{1
+# TODO: decide name
+def set(arg1):
+    """Set a value.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    pass
+
+
+def get(arg1):
+    """Get a value.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    pass
+
+
+# sqlite {{{1
+def create_sqlite_db(arg1):
+    """TODO: Docstring for create_sqlite_db.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    pass
+
+# redis {{{1
+
+# mongodb {{{1
---
+++ tests/state_tests.py
@@ -0,0 +1,10 @@
+"""
+state_tests.py
+"""
+
+from minicps.state import create_sqlite_db
+
+
+def test_create_sqlite_db():
+
+    pass
