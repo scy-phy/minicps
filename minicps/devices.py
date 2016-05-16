c1c29bb194a02a11bdf32c75d196943738793ccb
francozappa <antonioli.daniele@gmail.com>
2016-05-16 11:15:47 +0800

    Add PLC class and test

--- minicps/devices.py
+++ minicps/devices.py
@@ -25,3 +25,59 @@ class Device(object):
         self.protocol = protocol
         self.memory = memory
         self.disk = disk
+
+        self.start()
+
+    def start(self):
+        """Start a device."""
+
+        print "start: please override me"
+
+    def stop(self):
+        """Start a device."""
+
+        print "stop: please override me"
+
+
+class PLC(Device):
+
+    """Programmable Logic Controller.
+
+    PLC has control, monitor and network capabilites.
+
+    Usually they run a pre-loop initialization routine and then they enter a
+    main loop.
+    """
+
+    def start(self):
+
+        self.pre_loop()
+        self.main_loop()
+
+    # TODO
+    def stop(self):
+
+        pass
+
+    def pre_loop(self, sleep=0.5):
+        """PLC boot process.
+
+        :sleep: sleep n sec after it
+        """
+
+        print "pre_loop: please override me"
+        time.sleep(sleep)
+
+    def main_loop(self, sleep=0.5):
+        """PLC main loop.
+
+        :sleep: sleep n sec after each iteration
+        """
+
+        sec = 0
+        while(sec < 1):
+
+            print "main_loop: please override me"
+            time.sleep(sleep)
+
+            sec += 1
--- tests/devices_tests.py
+++ tests/devices_tests.py
@@ -2,11 +2,12 @@
 devices tests
 """
 
-from minicps.devices import Device
+from minicps.devices import Device, PLC
 
 
 def test_Device():
 
+    print
     device = Device(
         name='device',
         state='sqlite',
@@ -22,9 +23,27 @@ def test_Device():
             'TAG5': '5',
         })
 
+    print 'Device name: ', device.name
+    print 'Device state: ', device.state
+    print 'Device protocol: ', device.protocol
+    print 'Device memory: ', device.memory
+    print 'Device disk: ', device.disk
+
+
+def test_PLC():
+
     print
-    print device.name
-    print device.state
-    print device.protocol
-    print device.memory
-    print device.disk
+    device = PLC(
+        name='plc',
+        state='sqlite',
+        protocol='enip',
+        memory={
+            'TAG1': '1',
+            'TAG2': '2',
+        },
+        disk={
+            'TAG1': '1',
+            'TAG2': '2',
+            'TAG4': '4',
+            'TAG5': '5',
+        })
