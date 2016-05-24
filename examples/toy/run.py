753b1d43cef7b97a8e052afdb498e0c36dfb4fdb
francozappa <antonioli.daniele@gmail.com>
2016-05-24 16:24:35 +0800

    Move toy topology to topo.py

--- examples/toy/run.py
+++ examples/toy/run.py
@@ -7,39 +7,15 @@ import sys
 # TODO: find a nicer way to manage path
 sys.path.append(os.getcwd())
 
-from mininet.topo import Topo
 from mininet.net import Mininet
-
 from minicps.mcps import MiniCPS
-
-from examples.toy.utils import PLC1_MAC, PLC2_MAC
-from examples.toy.utils import PLC1_ADDR, PLC2_ADDR
-
-
-class ToyTopo(Topo):
-
-    def build(self):
-
-        switch = self.addSwitch('s1')
-
-        plc1 = self.addHost(
-            'plc1',
-            ip=PLC1_ADDR,
-            mac=PLC1_MAC)
-        self.addLink(plc1, switch)
-
-        plc2 = self.addHost(
-            'plc2',
-            ip=PLC2_ADDR,
-            mac=PLC2_MAC)
-        self.addLink(plc2, switch)
+from examples.toy.topo import ToyTopo
 
 
 if __name__ == "__main__":
 
     topo = ToyTopo()
-    net = Mininet(
-        topo=topo)
+    net = Mininet(topo=topo)
 
     minicps = MiniCPS(
         name='toy',
---
+++ examples/toy/topo.py
@@ -0,0 +1,23 @@
+from mininet.topo import Topo
+
+from examples.toy.utils import PLC1_MAC, PLC2_MAC
+from examples.toy.utils import PLC1_ADDR, PLC2_ADDR
+
+
+class ToyTopo(Topo):
+
+    def build(self):
+
+        switch = self.addSwitch('s1')
+
+        plc1 = self.addHost(
+            'plc1',
+            ip=PLC1_ADDR,
+            mac=PLC1_MAC)
+        self.addLink(plc1, switch)
+
+        plc2 = self.addHost(
+            'plc2',
+            ip=PLC2_ADDR,
+            mac=PLC2_MAC)
+        self.addLink(plc2, switch)
