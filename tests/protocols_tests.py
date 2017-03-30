4a8e693d9781f297cb1b6ea77d382336ca977da7
francozappa <antonioli.daniele@gmail.com>
2017-03-30 16:23:52 +0800

    TestModbusProtocol test_init passes

--- tests/protocols_tests.py
+++ tests/protocols_tests.py
@@ -256,18 +256,16 @@ class TestModbusProtocol():
     else:
         raise OSError
 
-    # TODO
-    @SkipTest
     def test_init(self):
 
         # TODO: add _stop_server
         modbus = ModbusProtocol(
             protocol=TestModbusProtocol.CLIENT_PROTOCOL)
-        eq_(modbus._name, 'modbustcp')
+        eq_(modbus._name, 'modbus')
         del modbus
         modbus = ModbusProtocol(
             protocol=TestModbusProtocol.CLIENT_SERVER_PROTOCOL)
-        eq_(modbus._name, 'modbustcp')
+        eq_(modbus._name, 'modbus')
 
     @SkipTest
     def test_server_stop(self):
@@ -280,19 +278,7 @@ class TestModbusProtocol():
         except Exception as error:
             print 'ERROR test_server_stop: ', error
 
-    # TODO
     @SkipTest
-    def test_server_start(self):
-
-        ADDRESS = 'localhost:44818'  # TEST port
-        TAGS = (('SENSOR1', 'INT'), ('ACTUATOR1', 'INT'))
-        try:
-            print "TEST: client has to kill the cpppo process."
-            ModbusProtocol._start_server(ADDRESS, TAGS)
-            # TODO: add _stop_server
-        except Exception as error:
-            print 'ERROR test_server_start: ', error
-
     def test_server_start_cmd(self):
 
         try:
@@ -304,102 +290,12 @@ class TestModbusProtocol():
 
     # TODO
     @SkipTest
-    def test_server_udp(self):
-
-        pass
-
-    # TODO
-    @SkipTest
-    def test_send_multikey(self):
-
-        enip = ModbusProtocol(
-            protocol=CLIENT_PROTOCOL)
-
-        TAGS = (('SENSOR1', 1, 'INT'), ('ACTUATOR1', 'INT'))
-        cmd = ModbusProtocol._start_server_cmd(tags=TAGS)
-        try:
-            server = subprocess.Popen(cmd, shell=False)
-
-            # write a multikey
-            what = ('SENSOR1', 1)
-            address = 'localhost:44818'
-            for value in range(5):
-                enip._send(what, value, address)
-
-            # write a single key
-            what = ('ACTUATOR1',)
-            address = 'localhost:44818'
-            for value in range(5):
-                enip._send(what, value, address)
-
-            ModbusProtocol._stop_server(server)
-
-        except Exception as error:
-            ModbusProtocol._stop_server(server)
-            print 'ERROR test_client: ', error
-
-    # TODO
-    @SkipTest
-    def test_receive_multikey(self):
-
-        enip = ModbusProtocol(
-            protocol=CLIENT_PROTOCOL)
-
-        TAGS = (('SENSOR1', 1, 'INT'), ('ACTUATOR1', 'INT'))
-        cmd = ModbusProtocol._start_server_cmd(tags=TAGS)
-        try:
-            server = subprocess.Popen(cmd, shell=False)
-
-            # read a multikey
-            what = ('SENSOR1', 1)
-            address = 'localhost:44818'
-            enip._receive(what, address)
-
-            # read a single key
-            what = ('ACTUATOR1',)
-            address = 'localhost:44818'
-            enip._receive(what, address)
-
-            ModbusProtocol._stop_server(server)
-
-        except Exception as error:
-            ModbusProtocol._stop_server(server)
-            print 'ERROR test_client: ', error
-
-    # TODO
-    @SkipTest
-    def test_client_server(self):
+    def test_server_start_stop(self):
 
         try:
-            enip = ModbusProtocol(
-                protocol=CLIENT_SERVER_PROTOCOL)
-
-            # read a multikey
-            what = ('SENSOR1', 1)
-            address = 'localhost:44818'
-            enip._receive(what, address)
-
-            # read a single key
-            what = ('ACTUATOR1',)
-            address = 'localhost:44818'
-            enip._receive(what, address)
-
-            # write a multikey
-            what = ('SENSOR1', 1)
-            address = 'localhost:44818'
-            for value in range(5):
-                enip._send(what, value, address)
-
-            # write a single key
-            what = ('ACTUATOR1',)
-            address = 'localhost:44818'
-            for value in range(5):
-                enip._send(what, value, address)
-
-            ModbusProtocol._stop_server(enip._server_subprocess)
+            ModbusProtocol._start_server(ADDRESS, TAGS)
 
         except Exception as error:
-            ModbusProtocol._stop_server(enip._server_subprocess)
-            print 'ERROR test_client_server: ', error
+            print 'ERROR test_server_start: ', error
 
 # }}}
