6b415c78b92dfb0bed48c3ce298be3f29382954b
francozappa <antonioli.daniele@gmail.com>
2017-07-06 17:41:05 +0800

    Use correct tags

--- examples/swat-ta/plc1.py
+++ examples/swat-ta/plc1.py
@@ -5,7 +5,9 @@ swat-ta plc1.py
 from minicps.devices import PLC
 from utils import PLC1_DATA, STATE, PLC1_PROTOCOL
 from utils import PLC_PERIOD_SEC
-from utils import IP, LIT_101_M, LIT_301_M, FIT_201_THRESH
+from utils import IP
+from utils import LIT_101_M, LIT_301_M, FIT_201_THRESH
+from utils import LIT101_1, MV101_1, P101_1
 
 import time
 
@@ -34,7 +36,7 @@ class SwatPLC1(PLC):
             # NOTE: lit101 [meters]
             lit101 = float(self.get(LIT101))
             print 'DEBUG plc1 lit101: %.5f' % lit101
-            self.send(LIT101, lit101, IP['plc1'])
+            self.send(LIT101_1, lit101, IP['plc1'])
 
             # NOTE: blocking read fit201 and lit301 interlocks
             fit201 = float(self.receive(FIT201_2, IP['plc2']))
@@ -46,16 +48,16 @@ class SwatPLC1(PLC):
 
             if lit101 <= LIT_101_M['L']:
                 # NOTE: close mv101
-                self.set(MV101, 1)
-                self.memory['MV101'] = 1
+                self.set(MV101_1, 1)
+                self.memory['MV101_1'] = 1
             if lit101 >= LIT_101_M['H']:
                 # NOTE: close mv101
-                self.set(MV101, 0)
-                self.memory['MV101'] = 0
+                self.set(MV101_1, 0)
+                self.memory['MV101_1'] = 0
             if lit101 <= LIT_101_M['LL']:
                 # NOTE: stop p101
-                self.set(P101, 0)
-                self.memory['P101'] = 0
+                self.set(P101_1, 0)
+                self.memory['P101_1'] = 0
                 # TODO: add alarm to state
                 print('ALARM: lit101 below LL: {}'.format(lit101))
             if lit101 >= LIT_101_M['HH']:
@@ -64,16 +66,16 @@ class SwatPLC1(PLC):
             # FIXME: no check about lit301 integrity
             if lit301 <= LIT301_1['L']:
                 # NOTE: start p101
-                self.set(P101, 1)
-                self.memory['P101'] = 1
+                self.set(P101_1, 1)
+                self.memory['P101_1'] = 1
             if lit301 >= LIT301_1['H'] or fit201 <= FIT_201_THRESH:
                 # NOTE: stop p101
-                self.set(P101, 0)
-                self.memory['P101'] = 0
+                self.set(P101_1, 0)
+                self.memory['P101_1'] = 0
 
             # NOTE: update internal enip server
-            self.send(MV101, self.memory['MV101'], IP['plc1'])
-            self.send(P101, self.memory['P101'], IP['plc1'])
+            self.send(MV101_1, self.memory['MV101_1'], IP['plc1'])
+            self.send(P101_1, self.memory['P101_1'], IP['plc1'])
 
             time.sleep(PLC_PERIOD_SEC)
 
