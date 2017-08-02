aacf164f786396c35af3c50558f544262af82d62
remmihsorp <ahnafsidd@gmail.com>
2017-08-02 19:09:57 +0800

    added Init class for generating scaffold

--- minicps/ui/commands.py
+++ minicps/ui/commands.py
@@ -2,15 +2,36 @@
 This file contains the commands that can be used (with parameters) both by cli
 and gui packages. For example the mcps click cli is using them as subcommands.
 
-This it the list of supported commands:
+This is the list of supported commands:
 
     - init
 """
+import os
+
+from template import Template
 
 class Init(object):
 
     """Docstring for Init. """
 
-    def __init__(self):
-        """TODO: to be defined. """
-        pass
+    def __init__(self, cwd):
+        self._cwd = cwd
+        self._template = Template
+
+    def default(self):
+        path = '{}/scaffold'.format(self._cwd)
+        os.makedirs(path)
+
+        for x in range(1,3):
+            with open('{}/plc{}.py'.format(path, x), 'w') as f:
+                name = self._template.device('PLC{}'.format(x))
+                f.write(name)
+
+        with open('{}/topo.py'.format(path), 'w') as f:
+            f.write(self._template.topology())
+
+        with open('{}/run.py'.format(path), 'w') as f:
+            f.write(self._template.run())
+
+        with open('{}/state.py'.format(path), 'w') as f:
+            f.write(self._template.state())
