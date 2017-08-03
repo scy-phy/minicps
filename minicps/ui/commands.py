e1c4ba1eee8df7c16c6ec9dfdd28f87337f53ae3
remmihsorp <ahnafsidd@gmail.com>
2017-08-03 16:21:40 +0800

    DRY'd code

--- minicps/ui/commands.py
+++ minicps/ui/commands.py
@@ -20,24 +20,31 @@ class Init(object):
     def make(self, _type="default", path=None):
         if _type == "default":
             self._template = TemplateFactory.getTemplate(_type)
-            self._default()
+            self._default(path)
         else:
-            raise NotImplementedError()
+            raise NotImplementedError("Only default is supported.")
 
-    def _default(self):
-        path = '{}/scaffold'.format(self._cwd)
-        os.makedirs(path)
+    def _default(self, path):
+        dir_path = '{}/{}'.format(self._cwd, path)
+        os.makedirs(dir_path)
 
         devices = self._template.devices(number=2)
         for idx in range(len(devices)):
-            with open('{}/plc{}.py'.format(path, (idx+1)), 'w') as f:
-                f.write(devices[idx])
+            self._create('{}/plc{}.py'.format(path, (idx+1)), devices[idx])
+            self._display("{path}/plc{idx}.py".format(folder=folder, idx=idx+1))
 
-        with open('{}/topo.py'.format(path), 'w') as f:
-            f.write(self._template.topology())
+        self._create('{}/run.py'.format(path), self._template.run())
+        self._create('{}/state.py'.format(path), self._template.state())
+        self._create('{}/topo.py'.format(path), self._template.topology())
 
-        with open('{}/run.py'.format(path), 'w') as f:
-            f.write(self._template.run())
+        self._display("{folder}/state.py".format(folder=folder))
+        self._display("{folder}/run.py".format(folder=folder))
+        self._display("{folder}/topo.py".format(folder=folder))
 
-        with open('{}/state.py'.format(path), 'w') as f:
-            f.write(self._template.state())
+    def _create(self, path, message):
+        with open(path, 'w') as f:
+            f.write(message)
+
+    def _display(self, message):
+        print "{:<5} create {}".format("", message)
+
