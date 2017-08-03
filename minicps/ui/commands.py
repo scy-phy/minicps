ac8d18d903b3d472cd2a948d3727c5ee32f3e7de
remmihsorp <ahnafsidd@gmail.com>
2017-08-03 17:07:03 +0800

    added custom path cli optional arg

--- minicps/ui/commands.py
+++ minicps/ui/commands.py
@@ -13,11 +13,12 @@ class Init(object):
 
     """Docstring for Init. """
 
-    def __init__(self, cwd):
-        self._cwd = cwd
+    def __init__(self):
         self._template = None
 
-    def make(self, _type="default", path=None):
+    def make(self, path, _type="default"):
+        os.makedirs(path)
+
         if _type == "default":
             self._template = TemplateFactory.getTemplate(_type)
             self._default(path)
@@ -25,21 +26,18 @@ class Init(object):
             raise NotImplementedError("Only default is supported.")
 
     def _default(self, path):
-        dir_path = '{}/{}'.format(self._cwd, path)
-        os.makedirs(dir_path)
-
         devices = self._template.devices(number=2)
         for idx in range(len(devices)):
             self._create('{}/plc{}.py'.format(path, (idx+1)), devices[idx])
-            self._display("{path}/plc{idx}.py".format(folder=folder, idx=idx+1))
+            self._display("{folder}/plc{idx}.py".format(folder=path, idx=idx+1))
 
         self._create('{}/run.py'.format(path), self._template.run())
         self._create('{}/state.py'.format(path), self._template.state())
         self._create('{}/topo.py'.format(path), self._template.topology())
 
-        self._display("{folder}/state.py".format(folder=folder))
-        self._display("{folder}/run.py".format(folder=folder))
-        self._display("{folder}/topo.py".format(folder=folder))
+        self._display("{folder}/state.py".format(folder=path))
+        self._display("{folder}/run.py".format(folder=path))
+        self._display("{folder}/topo.py".format(folder=path))
 
     def _create(self, path, message):
         with open(path, 'w') as f:
