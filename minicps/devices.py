e77e403ac9109e4daddc50693cc3df848da2ccb3
Carlos Cilleruelo Rodríguez <carlos.cilleruelo@alphasec.xyz>
2019-11-28 09:58:59 +0100

    First try to port minicps to python3

--- minicps/devices.py
+++ minicps/devices.py
@@ -177,14 +177,14 @@ class Device(object):
             # TODO: add parametric key serialization
             self._state = RedisState(self.state)
         else:
-            print 'ERROR: %s backend not supported.' % self.state
+            print('ERROR: %s backend not supported.' % self.state)
 
     # TODO: add optional process name for the server and log location
     def _init_protocol(self):
         """Bind device to network API."""
 
         if self.protocol is None:
-            print 'DEBUG: %s has no networking capabilities.' % self.name
+            print('DEBUG: %s has no networking capabilities.' % self.name)
             pass
         else:
             name = self.protocol['name']
@@ -193,17 +193,17 @@ class Device(object):
             elif name == 'modbus':
                 self._protocol = ModbusProtocol(self.protocol)
             else:
-                print 'ERROR: %s protocol not supported.' % self.protocol
+                print('ERROR: %s protocol not supported.' % self.protocol)
 
     def _start(self):
         """Start a device."""
 
-        print "TODO _start: please override me"
+        print("TODO _start: please override me")
 
     def _stop(self):
         """Start a device."""
 
-        print "TODO _stop: please override me"
+        print("TODO _stop: please override me")
 
     def set(self, what, value):
         """Set (write) a physical process state value.
@@ -298,7 +298,7 @@ class PLC(Device):
         :param float sleep: second[s] to sleep before returning
         """
 
-        print "TODO PLC pre_loop: please override me"
+        print("TODO PLC pre_loop: please override me")
         time.sleep(sleep)
 
     def main_loop(self, sleep=0.5):
@@ -310,7 +310,7 @@ class PLC(Device):
         sec = 0
         while(sec < 1):
 
-            print "TODO PLC main_loop: please override me"
+            print("TODO PLC main_loop: please override me")
             time.sleep(sleep)
 
             sec += 1
@@ -344,7 +344,7 @@ class HMI(Device):
         sec = 0
         while(sec < 1):
 
-            print "TODO HMI main_loop: please override me"
+            print("TODO HMI main_loop: please override me")
             time.sleep(sleep)
 
             sec += 1
@@ -384,7 +384,7 @@ class Tank(Device):
         :param float sleep: second[s] to sleep before returning
         """
 
-        print "TODO Tank pre_loop: please override me"
+        print("TODO Tank pre_loop: please override me")
 
     def main_loop(self, sleep=0.5):
         """Tank main loop.
@@ -395,7 +395,7 @@ class Tank(Device):
         sec = 0
         while(sec < 1):
 
-            print "TODO Tank main_loop: please override me"
+            print("TODO Tank main_loop: please override me")
             time.sleep(sleep)
 
             sec += 1
@@ -426,7 +426,7 @@ class SCADAServer(Device):
         :param float sleep: second[s] to sleep before returning
         """
 
-        print "TODO SCADAServer pre_loop: please override me"
+        print("TODO SCADAServer pre_loop: please override me")
         time.sleep(sleep)
 
     def main_loop(self, sleep=0.5):
@@ -438,7 +438,7 @@ class SCADAServer(Device):
         sec = 0
         while(sec < 1):
 
-            print "TODO SCADAServer main_loop: please override me"
+            print("TODO SCADAServer main_loop: please override me")
             time.sleep(sleep)
 
             sec += 1
@@ -469,7 +469,7 @@ class RTU(Device):
         :param float sleep: second[s] to sleep before returning
         """
 
-        print "TODO RTU pre_loop: please override me"
+        print("TODO RTU pre_loop: please override me")
         time.sleep(sleep)
 
     def main_loop(self, sleep=0.5):
@@ -481,7 +481,7 @@ class RTU(Device):
         sec = 0
         while(sec < 1):
 
-            print "TODO RTU main_loop: please override me"
+            print("TODO RTU main_loop: please override me")
             time.sleep(sleep)
 
             sec += 1
--- minicps/sdns.py
+++ minicps/sdns.py
@@ -1,175 +1,262 @@
 """
-MiniCPS Software Defined Networking (SDN) module.
+``states`` module.
 
-It contains OpenFlow data objects.
-
-It contains platform-specific SDN controller e.g., pox.
-POX prefixed ClassNames indicate controller coded into script/pox dir
-and symlinked to ~/pox/pox/forwarding dir.
-Minicps assumes that pox is cloned into your $HOME dir.
-For more information visit:
-https://openflow.stanford.edu/display/ONL/POX+Wiki
-
-By default Mininet runs Open vSwitch in OpenFlow mode,
-which requires an OpenFlow controller.
-Controller subclasses are started and stopped automatically by Mininet.
-RemoteController must be started and stopped by the user.
-Controller that enables learning switches doesn't work natively on
-topologies that contains loops and multiple paths (eg: fat trees)
-but they work fine with spanning tree topologies.
-
+MiniCPS uses prepared statements to speed-up the execution of the queries,
+and protect the database against SQL injection attacks.
 """
 
-from mininet.node import Controller
-
-# pox {{{1
-POX_PATH = '~/'
-
-POX = {
-    './pox.py openflow.of_01 --port=6633 --address=127.0.0.1' +
-    'log.level --DEBUG swat_controller',
-}
+import os
+import sqlite3
+# import redis
+# import pymongo
 
 
-def set_pox_opts(
-        components, info_level, logfile_opts,
-        log_format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'):
+class State(object):
 
-    """Generate a string with custom pox options.
+    """Base class."""
 
-    :components: dot notation paths (eg: forwarding.l2_learning web.webcore --port=8888)
-    :info_level: DEBUG, INFO, etc.
-    :logfile_opts: path and other options (eg: file.log,w to overwrite each time)
-    :returns: options string for ./pox.py command
+    def __init__(self, state):
+        """Init a State object.
 
+        For ``state`` format see the ``devices`` module docstring.
+
+        :param dict state: validated dict passed from a Device instance.
+        """
+
+        self._state = state
+
+    @classmethod
+    def _create(cls):
+        """Create a state instance.
+
+        Eg: create a MySQL db.
+
+        """
+        pass
+
+    @classmethod
+    def _init(cls):
+        """Initialize a state instance.
+
+        Eg: init MySQL db tables.
+
+        """
+        pass
+
+    @classmethod
+    def _delete(cls):
+        """Create a state instance.
+
+        Eg: remove a MySQL db.
+
+        """
+        pass
+
+    def _set(self, what, value):
+        """Set (write) a state value.
+
+        :what: tuple with field identifiers
+        :value: value
+        """
+
+        print("set: please override")
+
+    def _get(self, what):
+        """Get (read) a state value.
+
+        :what: (Immutable) tuple with field identifiers
+        """
+
+        print("get: please override")
+
+
+# sqlite {{{1
+# TODO: extend prepared statement to every SQL backend
+# TODO: single table or multiple tables to separate datatypes
+class SQLiteState(State):
+
+    """SQLite state manager.
+
+    SQLite uses TEXT data type instead of VARCHAR.
+
+    Client has to use ordered primary key fields to use get and set.
     """
-    info_level = info_level.upper()
-    pox_opts = '%s log.level --%s log --file=%s --format="%s" &' % (
-        components,
-        info_level, logfile_opts, log_format)
-    # print 'DEBUG:', opts
 
-    return pox_opts
+    def __init__(self, state):
+
+        super(SQLiteState, self).__init__(state)
+
+        self._name = self._state['name']
+        self._path = self._state['path']
+        self._value = 'value'  # TODO: add it to the device state dict
+        self._what = ()
+
+        self._init_what()
+
+        if not self._what:
+            raise ValueError('Primary key not found.')
+        else:
+            self._init_get_query()
+            self._init_set_query()
+
+    # TODO check :memory: opt to save db in main memory"
+    @classmethod
+    def _create(cls, db_name, schema):
+        """Create a sqlite db given a schema.
+
+        OVERWRITES db_name path by default.
+
+        :db_name: full or relative paths are supported
+        :schema: str containing the schema
+        """
+
+        with sqlite3.connect(db_name) as conn:
+            conn.executescript(schema)
+
+    @classmethod
+    def _init(cls, db_name, init_cmd):
+        """Initialize a sqlite database given commands.
+
+        :db_name: full or relative paths are supported
+        :init_cmd: initialization commands
+        """
+
+        with sqlite3.connect(db_name) as conn:
+            conn.executescript(init_cmd)
+
+    @classmethod
+    def _delete(cls, db_name):
+        """Delete a sqlite database given commands.
+
+        :db_name: full or relative paths are supported
+        """
+
+        try:
+            os.remove(db_name)
+
+        except OSError as e:
+            print('DEBUG %s do NOT exists in the filesystem.')
+
+    def _init_what(self):
+        """Save a ordered tuple of pk field names in self._what."""
+
+        # https://sqlite.org/pragma.html#pragma_table_info
+        query = "PRAGMA table_info(%s)" % self._name
+        # print "DEBUG query: ", query
+
+        with sqlite3.connect(self._path) as conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(query)
+                table_info = cursor.fetchall()
+                # print "DEBUG table_info: ", table_info
+
+                # last tuple element
+                pks = []
+                for field in table_info:
+                    if field[-1] > 0:
+                        # print 'DEBUG pk field: ', field
+                        pks.append(field)
+
+                if not pks:
+                    print("ERROR: please provide at least 1 primary key")
+                else:
+                    # sort by pk order
+                    pks.sort(key=lambda x: x[5])
+                    # print 'DEBUG sorted pks: ', pks
+
+                    what_list = []
+                    for pk in pks:
+                        what_list.append(pk[1])
+                    # print 'DEBUG what list: ', what_list
+
+                    self._what = tuple(what_list)
+                    # print 'DEBUG self._what: ', self._what
+
+            except sqlite3.Error as e:
+                print('ERROR: %s: ' % e.args[0])
+
+    def _init_get_query(self):
+        """Use prepared statement."""
+
+        get_query = 'SELECT %s FROM %s WHERE %s = ?' % (
+            self._value,
+            self._name,
+            self._what[0])
+
+        # for composite pk
+        for pk in self._what[1:]:
+            get_query += ' AND %s = ?' % (
+                pk)
+
+        print('DEBUG get_query:', get_query)
+        self._get_query = get_query
+
+    def _init_set_query(self):
+        """Use prepared statements."""
+
+        set_query = 'UPDATE %s SET %s = ? WHERE %s = ?' % (
+            self._name,
+            self._value,
+            self._what[0])
+
+        # for composite pk
+        for pk in self._what[1:]:
+            set_query += ' AND %s = ?' % (
+                pk)
+
+        print('DEBUG set_query:', set_query)
+        self._set_query = set_query
+
+    # TODO: return result of cursor.execute
+    def _set(self, what, value):
+        """Returns setted value.
+
+        ``value``'s type is not checked, the client has to specify the correct
+        one.
+
+        what_list overwrites the given what tuple,
+        eg new what tuple: ``(value, what[0], what[1], ...)``
+        """
+        what_list = [value]
+
+        for pk in what:
+            what_list.append(pk)
+
+        what = tuple(what_list)
+        # print 'DEBUG set what: ', what
+
+        with sqlite3.connect(self._path) as conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(self._set_query, what)
+                conn.commit()
+                return value
+
+            except sqlite3.Error as e:
+                print('_set ERROR: %s: ' % e.args[0])
+
+    def _get(self, what):
+        """Returns the first element of the result tuple."""
+
+        with sqlite3.connect(self._path) as conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(self._get_query, what)
+                record = cursor.fetchone()
+                return record[0]
+
+            except sqlite3.Error as e:
+                print('_get ERROR: %s: ' % e.args[0])
 
 
-class POXL2Pairs(Controller):
+# redis {{{1
+class RedisState(State):
 
-    """MAC-learning controller."""
+    """Redis state manager."""
 
-    def start(self):
-        self.pox = '%s/pox/pox.py' % (POX_PATH)
-        pox_opts = set_pox_opts(
-            'forwarding.l2_pairs', 'DEBUG', 'logs/' +
-            type(self).__name__ + '.log,w')
-        self.cmd(self.pox, pox_opts)
+    pass
 
-    def stop(self):
-        self.cmd('kill %' + self.pox)
+# mongodb {{{1
 
-
-class POXL2Learning(Controller):
-
-    """Build a controller able to update switches
-    flow tables according to flow-based criteria
-    (not only MAC-based flow matching)."""
-
-    def start(self):
-        self.pox = '%s/pox/pox.py' % (POX_PATH)
-        pox_opts = set_pox_opts(
-            'forwarding.l2_learning', 'DEBUG', 'logs/' +
-            type(self).__name__ + '.log,w')
-        self.cmd(self.pox, pox_opts)
-
-    def stop(self):
-        self.cmd('kill %' + self.pox)
-
-
-class POXProva(Controller):
-
-    """Use it to test components using POX_PATH."""
-
-    def start(self):
-        POX_PATH = 'hub'  # pox/ext/ dir
-
-        self.pox = '%s/pox/pox.py' % (POX_PATH)
-        pox_opts = set_pox_opts(
-            POX_PATH, 'DEBUG', 'logs/' +
-            type(self).__name__ + '.log,w')
-        self.cmd(self.pox, pox_opts)
-        # self.cmd(
-        #     self.pox,
-        #     'forwarding.prova log.level --DEBUG log --file=./logs/pox.log &')
-
-    def stop(self):
-        self.cmd('kill %' + self.pox)
-
-
-class POXSwat(Controller):
-
-    """Build a controller based on temp/antiarppoison.py"""
-
-    def start(self):
-        self.pox = '%s/pox/pox.py' % (POX_PATH)
-        pox_opts = set_pox_opts(
-            'swat_controller', 'DEBUG', 'logs/' +
-            type(self).__name__ + '.log,w')
-        self.cmd(self.pox, pox_opts)
-
-    def stop(self):
-        self.cmd('kill %' + self.pox)
-
-
-class POXAntiArpPoison(Controller):
-
-    """Build a controller based on temp/antiarppoison.py"""
-
-    def start(self):
-        self.pox = '%s/pox/pox.py' % (POX_PATH)
-        pox_opts = set_pox_opts(
-            'antiarppoison', 'DEBUG', 'logs/' +
-            type(self).__name__ + '.log,w')
-        self.cmd(self.pox, pox_opts)
-
-    def stop(self):
-        self.cmd('kill %' + self.pox)
-
-
-# Openflow {{{1
-OF_MISC = {
-    'user_switch': 'user',
-    'kernel_switch': 'ovsk',
-    'controller_port': 6633,
-    'switch_debug_port': 6634,
-    'flood_port': 65531,
-}
-
-OF10_MSG_TYPES = {
-    0: 'OFPT_HELLO',  # Symmetric
-    1: 'OFPT_ERROR',  # Symmetric
-    2: 'OFPT_ECHO_REQUEST',  # Symmetric
-    3: 'OFPT_ECHO_REPLY',  # Symmetric
-    4: 'OFPT_VENDOR',  # Symmetric
-
-    5: 'OFPT_FEATURES_REQUEST',  # Controller -> Switch
-    6: 'OFPT_FEATURES_REPLY',  # Switch -> Controller
-    7: 'OFPT_GET_CONFIG_REQUEST',  # Controller -> Switch
-    8: 'OFPT_GET_CONFIG_REPLY',  # Switch -> Controller
-    9: 'OFPT_SET_CONFIG',  # Controller -> Switch
-
-    10: 'OFPT_PACKET_IN',  # Async, Switch -> Controller
-    11: 'OFPT_FLOW_REMOVED',  # Async, Switch -> Controller
-    12: 'OFPT_PORT_STATUS',  # Async,  Switch -> Controller
-
-    13: 'OFPT_PACKET_OUT',  # Controller -> Switch
-    14: 'OFPT_FLOW_MOD',  # Controller -> Switch
-    15: 'OFPT_PORT_MOD',  # Controller -> Switch
-
-    16: 'OFPT_STATS_REQUEST',  # Controller -> Switch
-    17: 'OFPT_STATS_REPLY',  # Switch -> Controller
-
-    18: 'OFPT_BARRIER_REQUEST',  # Controller -> Switch
-    19: 'OFPT_BARRIER_REPLY',  # Switch -> Controller
-
-    20: 'OFPT_QUEUE_GET_CONFIG_REQUEST',  # Controller -> Switch
-    21: 'OFPT_QUEUE_GET_CONFIG_REPLY',  # Switch -> Controller
-}
+# spreasheet/csv {{{1
