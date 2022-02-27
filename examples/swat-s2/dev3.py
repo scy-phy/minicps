9884c6e04c4a496f980b6d800ef8635a91e91f4e
Sebastian Kosak <sebastian@kosaks.de>
2022-02-27 13:36:10 +0100

    t

--- examples/swat-s2/dev3.py
+++ examples/swat-s2/dev3.py
@@ -31,7 +31,8 @@ class SwatDev3(IODevice):
     def pre_loop(self, sleep=0.1):
         print 'DEBUG: swat-s2 dev2 enters pre_loop'
         start_level = 0.500
-        time.sleep(60)        self.set(LIT101, start_level)
+        time.sleep(60)        
+        self.set(LIT101, start_level)
         self.send(LIT101_1, start_level, PLC1_ADDR)
         self.send(LIT101, start_level, DEV3_ADDR)
         time.sleep(sleep)
