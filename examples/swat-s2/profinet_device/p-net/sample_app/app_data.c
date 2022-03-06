492310ddbee33f2687ea989b183fcecc5caee01b
Sebastian Kosak <sebastian@kosaks.de>
2022-03-06 12:26:25 +0100

    test

--- examples/swat-s2/profinet_device/p-net/sample_app/app_data.c
+++ examples/swat-s2/profinet_device/p-net/sample_app/app_data.c
@@ -93,7 +93,6 @@ uint8_t * app_data_get_input_data (
       *iops = PNET_IOXS_GOOD;
       // printf ("Input n-data received!\n");
       // printf ("Count = %u!\n", counter);
-
       // inputdata_4[0] = counter;
       // printf("Input data 4: %u%u%u%u\n", inputdata_4[0], inputdata_4[1], inputdata_4[2], inputdata_4[3]);
 
