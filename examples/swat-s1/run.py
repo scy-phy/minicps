c336ed2d1b83d8bb26f98da48ba108506bd7d16c
Nils Ole Tippenhauer <noleti@gmx.de>
2022-07-16 17:33:35 +0200

    redirect stderr and stdout of hosts in example to log files

--- examples/swat-s1/run.py
+++ examples/swat-s1/run.py
@@ -29,12 +29,11 @@ class SwatS1CPS(MiniCPS):
             'plc1', 'plc2', 'plc3', 's1')
 
         # SPHINX_SWAT_TUTORIAL RUN(
-        plc2.cmd(sys.executable + ' plc2.py &')
-        plc3.cmd(sys.executable + ' plc3.py &')
-        plc1.cmd(sys.executable + ' plc1.py &')
-        s1.cmd(sys.executable + ' physical_process.py &')
+        plc2.cmd(sys.executable + ' -u ' +' plc2.py &> logs/plc2.log &')
+        plc3.cmd(sys.executable + ' -u ' + ' plc3.py  &> logs/plc3.log &')
+        plc1.cmd(sys.executable + ' -u ' + ' plc1.py  &> logs/plc1.log &')
+        s1.cmd(sys.executable + ' -u ' + ' physical_process.py  &> logs/process.log &')
         # SPHINX_SWAT_TUTORIAL RUN)
-
         CLI(self.net)
 
         net.stop()
